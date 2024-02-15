81a419f04092536aad686eba2deff5336c132e35
theryanfo <ryan.s.fordham@gmail.com>
2024-02-15 11:53:05 -0800

    Lab wk 6

--- grade.sh
+++ grade.sh
@@ -1,16 +1,51 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
 rm -rf student-submission
 rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+#1. Clone Submission
+
+git clone $1 student-submission 2> clone-output.txt
 echo 'Finished cloning'
 
+# 2/3. Check correct file submission & cp file into grading-area/
 
-# Draw a picture/take notes on the directory structure that's set up after
-# getting to this point
+if [ -f student-submission/ListExamples.java ]
+then
+    cp student-submission/ListExamples.java grading-area/
+    cp TestListExamples.java grading-area/
+else 
+    echo "Missing ListExamples.java, did you forget the file or misname it?"
+    exit 1
+fi
 
-# Then, add here code to compile and run, and do any post-processing of the
-# tests
+# 4. Compile tests and student's code from grading-area/
+
+cd grading-area/
+CPATH=".;../lib/hamcrest-core-1.3.jar;../lib/junit-4.13.2.jar"
+javac -cp $CPATH *.java
+
+if [ $? -ne 0 ]
+then 
+    echo "Program failed to compile, see compile error above"
+    exit 1
+fi
+
+echo "Compile successful"
+
+# 5. Run the tests and report the grade
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+testline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+echo "$testline"
+
+if [ $(grep -o "OK" junit-output.txt | tail -n 2 | head -n 1) == "OK" ]
+then
+    tests=$(echo "$testline" | awk -F'[(]' '{print $2}')
+    echo "$tests"
+    # passed=$(cat "$testline" | awk -F'[(]' '{print $2}')
+    echo "Perfect!"
+    # echo "$passed"
+fi
